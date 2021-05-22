165f77adc99025fc042308a2a75565d4d03201ec
Nitesh Kumar <59920892+niteshkumar191917@users.noreply.github.com>
2021-05-22 19:57:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class time
 {
-    public:
+    private:
     
     int hour,minute;
     
@@ -14,36 +14,29 @@ class time
     cin>>hour;
     cout<<"minute =";
     cin>>minute;
-    };
+    }
     
-
-    void Friend(time t1,time t2)
-    {
-        
-        minute = t1.minute + t2.minute;
-        hour = t1.hour + t2.hour + minute/60;
-        minute %=60;
-
-    };
-
-     
-    void displaydata(time t1,time t2)
-    {
-             cout<<"your time 1 is: "<<t1.hour<<":"<<t1.minute<<endl;
-             cout<<"your time 2 is: "<<t2.hour<<":"<<t2.minute<<endl;
-             cout<<"      both time add...."<<endl;
-             cout<<"after adding both time"<<hour<<":"<<minute<<endl;
-    };
-    
-
+friend void add_time(time t1, time t2);
 
 };
+void add_time(time t1, time t2)
+{
+int minute = t1.minute + t2.minute;
+int hour = t1.hour + t2.hour + minute/60;
+minute %=60;
+cout<<"your time 1 is:"<<t1.hour<<":"<<t1.minute<<endl;
+cout<<"your time 2 is: "<<t2.hour<<":"<<t2.minute<<endl;
+cout<<" both time add...."<<endl;
+cout<<"after adding both time"<<hour<<":"<<minute<<endl;
+
+}
+
+}
 int main()
 {
-    time t1,t2,t3;
+    time t1,t2;
      t1.getdata();
      t2.getdata();
-     t3.Friend(t1,t2);
-     t3.displaydata(t1,t2);
+     add_time(t1,t2);
     return 0;
 }
